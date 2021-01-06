4978ad47ae904a9027015032d4ca6ee037bb8763
Muskan <42966101+LeoYoona@users.noreply.github.com>
2021-01-06 18:10:45 +0100

    oi

--- dboard/src/components/Form.js
+++ dboard/src/components/Form.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class form extends Component {
-    render() {
-        return (
-            <form>
-                <div>
-                   <label>
-                Username
-            </label>
-            <input type='text' /> 
-                </div>
-            </form>
-            
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      comments: "",
+    };
+  }
+  handleUSernameChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handleCommentsChange = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+  handleSubmit= (event) => {
+    alert(`${this.state.username} ${this.state.comments}`)
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUSernameChange}
+          />
+        </div>
+        <div>
+          <label>Comments</label>
+          <textarea
+            value={this.state.comments}
+            onChange={this.handleCommentsChange}
+          ></textarea>
+        </div>
+        <button type="submit"> submit</button>
+      </form>
+    );
+  }
 }
 
-export default form
+export default form;
